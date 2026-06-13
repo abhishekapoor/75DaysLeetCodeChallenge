e50e8c10fe7017ee8de121eea1065d8b88fde1bf
Abhishek Kapoor <abhishek832903@gmail.com>
2026-06-13 09:43:17 +0530

    Time: 2 ms (96.85%), Space: 46.7 MB (33.48%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<words.length; i++){
+            String s = words[i];
+            int sum = 0;
+            for(char c : s.toCharArray()){
+                sum += weights[c-'a'];
+            }
+            char c = (char) ('a' + Math.abs((sum % 26)-25));
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
